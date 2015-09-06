68b8037c2ffef3edb16689d26fd878ad99b1686f
ynaghuangsheng <260287826@qq.com>
2015-09-06 16:05:26 +0800

    增加 前端浮动

--- Public/home/js/web.js
+++ Public/home/js/web.js
@@ -4,39 +4,63 @@ $(function () {
            },function(){
         	   $(this).removeClass("active");
               });
-	    
-	    var public = checkbrowse(); 
-        var showeffect = ""; 
-        if ((public['is'] == 'msie' && public['ver'] < 8.0)) { 
-           showeffect = "show";
-        } else { 
-           showeffect = "fadeIn";
-        } 
-        
-            $("img").lazyload({ 
-            placeholder: "http://demo.jb51.net/js/2011/lazyload/Js/lazyload/grey.gif", 
-            effect: showeffect, 
-            failurelimit: 10 
-           }) 
-       
-	    
-	    
-	    
+	    $(".lazy").lazyload({ placeholder: "images/blue-loading.gif", effect:"fadeIn",threshold:200 });
+	    floatScroll();    
 });
- 
 
-function checkbrowse() { 
-var ua = navigator.userAgent.toLowerCase(); 
-var is = (ua.match(/\b(chrome|opera|safari|msie|firefox)\b/) || ['', 'mozilla'])[1]; 
-var r = '(?:' + is + '|version)[\\/: ]([\\d.]+)'; 
-var v = (ua.match(new RegExp(r)) || [])[1]; 
-jQuery.browser.is = is; 
-jQuery.browser.ver = v; 
-return { 
-'is': jQuery.browser.is, 
-'ver': jQuery.browser.ver 
-} 
-} 
+
+function floatScroll(){
+    //预定义浮动的html代码
+    var dox_html = '<div id="topBox"><div  class="li" id="gotop" ><a class="gotop"></a></div>';
+    	dox_html += '<div  class="li" id="weibo" ><a class="weibo"></a><div class="weibo_box"><p>扫描二维码关注我乐购</p><p><img src="/Public/home/images/weibo2wg.png"/></p><p><div class="h"><i></i><span>官方微博</span></div></p></div></div>';
+    	dox_html += '<div  class="li" id="shoucang" ><a class="shoucang"></a></div>';
+    	dox_html += '<div  class="li end" id="kefu" ><a class="kefu"></a></div></div>';
+    //动态插入页面topBox的元素
+    $("body").append(dox_html);
+    $(document).ready(function(e) {
+    	gotopshow();
+    	$('#topBox #gotop').click(
+    		 function(){$('html,body').animate({scrollTop:0},700);}
+    	);
+    	$('#topBox #shoucang').click(
+    			function(){AddFavorite('我乐购','http://56gou.com');}
+    	);
+    	$('#topBox .li').hover(
+    	        function(){$(this).addClass("hover");},
+    	        function(){$(this).removeClass("hover");}
+    	);
+    });
+
+    $(window).scroll(function(e){
+    	gotopshow();		
+    });
+    
+};
+ 
+function AddFavorite(title,url){
+	  url=encodeURI(url);
+	  var ctrl=(navigator.userAgent.toLowerCase()).indexOf('mac')!=-1?'Command/Cmd': 'CTRL';
+	  try{ 
+	  window.external.addFavorite(url, title);
+      }catch(e) {  
+      try{  
+	   window.sidebar.addPanel(title, url, "");
+      }catch (e) {  
+	   alert("加入收藏失败，请使用" + ctrl + "+D进行添加,或手动在浏览器里进行设置."); 
+	  }
+      }
+}
+
+function gotopshow(){
+	h = $(window).height();
+	t = $(document).scrollTop();
+	if(t > h){
+		$('#topBox #gotop').fadeIn('slow');
+	}else{
+		$('#topBox #gotop').fadeOut('slow');
+	}
+}
+
 
 var carousel_index = function(){
 	if($(".banner li").size() == 1) $(".banner li").eq(0).show();
