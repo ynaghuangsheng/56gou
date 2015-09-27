7791682cafd73331004b32ca8d5e8e92e210ac44
ynaghuangsheng <260287826@qq.com>
2015-09-27 17:36:14 +0800

    home 代码整理

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -2,39 +2,50 @@
 class IndexController extends Controller {
 	 
 	public function index(){
-           $this->is404();
-		   $list=$this->model->goodsSelect(!empty($_REQUEST['page'])?$_REQUEST['page']:1,40);
-           $data['total_rows']=$this->model->count;
-           $data['list_rows']=40;
-           $data['now_page']= !empty($_REQUEST['page'])?$_REQUEST['page']:1;
-           $data['url']='/discover/page/{page}';
-           $page=new Page($data);unset($data);
-           $page=$page->show();
-           $this->assign('list',$list);unset($list);
-		   $this->assign('page',$page);unset($page);
+		
+        $this->is404();
+        
+        //获取当前页码
+		$now_page=!empty($_REQUEST['page'])?$_REQUEST['page']:1;
+		$list_rows=40;//设置每页显示条数
+		//查询数据
+		$list=$this->model->goodsSelect($now_page,$list_rows);
+        
+		$data['total_rows']=$this->model->count;//得到数据的总条数
+        $data['list_rows']=$list_rows;//每页显示条数
+        $data['now_page']=$now_page;//当前页码
+        $data['url']='/discover/page/{page}';//设置翻页url
+        //实例化Page
+        $page=new Page($data);unset($data);
+        $page=$page->show();//获得翻页集
+        //模板赋值
+        $this->assign('list',$list);unset($list);
+		$this->assign('page',$page);unset($page);
+		
+		
+		$yifuData=$this->model->typeSelect(10,'`cid`=1');//查询推荐的衣服
+		$this->assign('yifuData',$yifuData);//模板赋值
 		   
-		   $yifuData=$this->model->typeSelect(10,'`cid`=1');
-		   $this->assign('yifuData',$yifuData);
+		$xieziData=$this->model->typeSelect(10,'`cid`=2');//查询推荐的鞋子
+		$this->assign('xieziData',$xieziData);//模板赋值
 		   
-		   $xieziData=$this->model->typeSelect(10,'`cid`=2');
-		   $this->assign('xieziData',$xieziData);
+		$baobaoData=$this->model->typeSelect(10,'`cid`=3');//查询推荐的包包
+		$this->assign('baobaoData',$baobaoData);//模板赋值
 		   
-		   $baobaoData=$this->model->typeSelect(10,'`cid`=3');
-		   $this->assign('baobaoData',$baobaoData);
+		$peishiData=$this->model->typeSelect(10,'`cid`=4');//查询推荐的配饰
+		$this->assign('peishiData',$peishiData);//模板赋值
 		   
-		   $peishiData=$this->model->typeSelect(10,'`cid`=4');
-		   $this->assign('peishiData',$peishiData);
+		$linkData=$this->model->linkSelect();//查询友链
+		$this->assign('link',$linkData);//模板赋值
+		
+		//设置头部 
+		$head['title']='我乐购,折扣购物,打折购物,专业的女性时尚品牌商品导购网';
+		$head['key']='我乐购,56购物网 ,特卖,折扣,打折';
+		$head['des']='我乐购,汇聚淘宝、天猫等各大购物商城最优质的折扣商品,每天为你推荐最新的名牌女装、品牌女包、高档女装、潮流服饰、美容护肤品等折扣信息-享折扣就上56购';
+		$this->assign('head',$head);//模板赋值
+		$this->assign('index','index');//设置导航游标
 		   
-		   $linkData=$this->model->linkSelect();
-		   $this->assign('link',$linkData);
-		   
-		   $head['title']='我乐购,折扣购物,打折购物,专业的女性时尚品牌商品导购网';
-		   $head['key']='我乐购,56购物网 ,特卖,折扣,打折';
-		   $head['des']='我乐购,汇聚淘宝、天猫等各大购物商城最优质的折扣商品,每天为你推荐最新的名牌女装、品牌女包、高档女装、潮流服饰、美容护肤品等折扣信息-享折扣就上56购';
-		   $this->assign('head',$head);
-		   $this->assign('index','index');
-		   
-		   $this->display('index.htpl');
+		$this->display('index.htpl');//模板输出
 	}
 	
 	public function is404(){
--- Home/Controller/XieziController.class.php
+++ Home/Controller/XieziController.class.php
@@ -1,36 +1,67 @@
 <?php
 class XieziController extends Controller {
-	//列表
+    //列表
 	public function index(){
-           $no_page=isset($_REQUEST['page'])?$_REQUEST['page']:1;
-           $count=40;
-           $url='/xiezi/';
-	       $where='`cid`=2 ';
-           $tag=isset($_REQUEST['tag'])?urldecode($_REQUEST['tag']):false;
-           if($tag){
-           	$url.=urlencode($tag).'/';
-           	$where.='and `title` like \'%'.$tag.'%\'';
-           	$head['title']="{$tag}:休闲时尚{$tag}品牌,{$tag}搭配图片|{$tag}款式,价格,折扣,优惠-我乐购";
-		    $head['key']="{$tag},{$tag}品牌,{$tag}款式,{$tag}图片,{$tag}搭配,{$tag}折扣,{$tag}优惠";
-		    $head['des']="{$tag}专栏精选精美设计的{$tag}款式及{$tag}品牌,教你如果如何搭配{$tag},并提供新款{$tag}资讯,和最新的{$tag}价格折扣优惠信息.-我乐购";
-           }else{
-           	$seo=$this->model->typeSelect();
-            $head['title']=$seo['seo_title'];
-            $head['key']=$seo['seo_key'];
-            $head['des']=$seo['seo_des'];
-           }
-		   $list=$this->model->Select($no_page,$count,$where);
-           $data['total_rows']=$this->model->count;
-           $data['list_rows']=$count;
-           $data['now_page']=$no_page;
-           $data['url']=$url.'{page}/';
-           $page=new Page($data);
-           $page=$page->show();
-		   $this->assign('head',$head);
-		   $this->assign('index','xiezi');
-		   $this->assign('list',$list);
-		   $this->assign('page',$page);
-		   $this->display('index.htpl');
+		
+		$now_page=isset($_REQUEST['page'])?$_REQUEST['page']:1;//获取当前页码
+        $list_rows=40;//设置每页显示条数
+        $url='/xiezi/';//设置默认Url
+	    $where='`cid`=2 ';//设置默认查询条件
+
+        //查询seo信息  
+        $seo=$this->model->typeSelect();
+        $head['title']=$seo['seo_title'];
+        $head['key']=$seo['seo_key'];
+        $head['des']=$seo['seo_des'];
+        //查询商品数据
+		$list=$this->model->Select($now_page,$list_rows,$where);
+		
+        $data['total_rows']=$this->model->count;//设置数据的总条数
+        $data['list_rows']=$list_rows;//每页显示条数
+        $data['now_page']=$now_page;//当前页码
+        $data['url']=$url.'{page}/';//设置翻页url
+        $page=new Page($data);
+        $page=$page->show();//获得翻页集
+        
+        //模板赋值
+		$this->assign('index','xiezi');//设置导航游标
+		$this->assign('list',$list);
+		$this->assign('page',$page);
+		$this->assign('head',$head);
+		   
+		$this->display('index.htpl');
 		
 	}
+	public function tag(){
+		
+		$now_page=isset($_REQUEST['page'])?$_REQUEST['page']:1;//获取当前页码
+		$list_rows=40;//设置每页显示条数
+		$tag=urldecode($_REQUEST['tag']);//获取当前细类
+        $url='/xiezi/'.urlencode($tag).'/';//设置默认Url
+	    $where="`cid`=2 and `title` like '%{$tag}%'";//设置查询条件
+	    //设置头部信息
+	    $head['title']="{$tag}:休闲时尚{$tag}品牌,{$tag}搭配图片|{$tag}款式,价格,折扣,优惠-我乐购";
+		$head['key']="{$tag},{$tag}品牌,{$tag}款式,{$tag}图片,{$tag}搭配,{$tag}折扣,{$tag}优惠";
+		$head['des']="{$tag}专栏精选精美设计的{$tag}款式及{$tag}品牌,教你如果如何搭配{$tag},并提供新款{$tag}资讯,和最新的{$tag}价格折扣优惠信息.-我乐购";
+		//查询数据
+		$list=$this->model->Select($now_page,$list_rows,$where);
+		
+        $data['total_rows']=$this->model->count;//设置数据的总条数
+        $data['list_rows']=$list_rows;//每页显示条数
+        $data['now_page']=$now_page;//当前页码
+        $data['url']=$url.'{page}/';//设置翻页url
+        $page=new Page($data);
+        $page=$page->show();//获得翻页集
+        
+		//模板赋值
+        $this->assign('index','xiezi');//设置导航游标
+		$this->assign('list',$list);
+		$this->assign('page',$page);
+		$this->assign('head',$head);
+		   
+		$this->display('index.htpl');
+		
+	}
+	
+	
 }
