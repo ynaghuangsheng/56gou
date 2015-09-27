2ab45809065112e15000ce6eeff2d4fa50364bb7
ynaghuangsheng <260287826@qq.com>
2015-09-27 22:51:58 +0800

    增加wap首页部份功能

--- Wap/Controller/AjaxController.class.php
+++ Wap/Controller/AjaxController.class.php
@@ -6,7 +6,13 @@ class AjaxController extends Controller {
 		$now_page=isset($_REQUEST['page'])?$_REQUEST['page']:2;
 		$cid=isset($_REQUEST['cid'])?$_REQUEST['cid']:null;
 		$tag=isset($_REQUEST['tag'])?urldecode($_REQUEST['tag']):null;
+		$tuijian=isset($_REQUEST['tuijian'])?$_REQUEST['tuijian']:null;
+		$ju=isset($_REQUEST['ju'])?$_REQUEST['ju']:null;
+		$guang=isset($_REQUEST['guang'])?$_REQUEST['guang']:null;
 		$where='';
+		$order='id desc';//设置默认排序
+		if(!is_null($tuijian))$order='`index` desc,id desc ';
+		if(!is_null($guang))$order='`tuijian` desc,id desc ';
 		if(!is_null($cid))$where="`cid`={$cid}";
 		if(!is_null($tag)){
 			if($where!==''){
@@ -15,8 +21,22 @@ class AjaxController extends Controller {
 				$where.="`title` like '%{$tag}%'";
 			}
 		}
+	    if(!is_null($ju)){
+			if($where!==''){
+				$where=' and `zk_price`<10 and `zk_price`>9';
+			}else{
+				$where=' `zk_price`<10 and `zk_price`>9';
+			}
+		}
+	    if(!is_null($tuijian)){
+			if($where!==''){
+				$where=' and `cid`<3';
+			}else{
+				$where='`cid`<3';
+			}
+		}
 		//查询产品数据
-		$list=$this->model->goodsSelect($now_page,$list_rows,$where);
+		$list=$this->model->goodsSelect($now_page,$list_rows,$where,$order);
 		$total_rows=$this->model->count;//产品总条数
 		if($list){
 		  $array = array( 'msg'=>true, 'data'=>$list,'total_rows'=>$total_rows );
--- Wap/Controller/IndexController.class.php
+++ Wap/Controller/IndexController.class.php
@@ -18,4 +18,57 @@ class IndexController extends Controller {
 
 	}
 	
+    public function tuijian(){
+		
+		$list_rows=C('WAP_LIST_ROWS');//设置每次获取条数
+		$where='`cid`<3';
+		$order='`index` desc,id desc ';
+		//查询产品数据
+		$list=$this->model->goodsSelect(1,$list_rows,$where,$order);
+		$total_rows=$this->model->count;//产品总条数
+        
+        //模板赋值
+        $this->assign('index','index');//设置导航游标
+        $this->assign('tag','tuijian');//设置导航游标
+        $this->assign('ajax_url','/m/ajax?tuijian=index');//设置加载商品地址
+        $this->assign('list',$list);unset($list);
+		$this->display('index.htpl');
+
+   }
+   
+   public function ju(){
+		
+		$list_rows=C('WAP_LIST_ROWS');//设置每次获取条数
+		$where='`zk_price`<10 and `zk_price`>9';
+		//查询产品数据
+		$list=$this->model->goodsSelect(1,$list_rows,$where);
+		$total_rows=$this->model->count;//产品总条数
+        
+        //模板赋值
+        $this->assign('index','index');//设置导航游标
+        $this->assign('tag','ju');//设置导航游标
+        $this->assign('ajax_url','/m/ajax?ju=index');//设置加载商品地址
+        $this->assign('list',$list);unset($list);
+		$this->display('index.htpl');
+
+   }
+   
+   public function guang(){
+		
+		$list_rows=C('WAP_LIST_ROWS');//设置每次获取条数
+		$where='';
+		$order='`tuijian` desc,id desc ';
+		//查询产品数据
+		$list=$this->model->goodsSelect(1,$list_rows,$where,$order);
+		$total_rows=$this->model->count;//产品总条数
+        
+        //模板赋值
+        $this->assign('index','index');//设置导航游标
+        $this->assign('tag','guang');//设置导航游标
+        $this->assign('ajax_url','/m/ajax?guang=index');//设置加载商品地址
+        $this->assign('list',$list);unset($list);
+		$this->display('guang.htpl');
+
+   }
+	
 }
