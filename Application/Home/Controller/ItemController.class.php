db00dbfe1029e1dbbf736dd1834bcfe847809741
ynaghuangsheng <260287826@qq.com>
2016-03-10 23:54:51 +0800

    为成人用品不让收录 而改造

--- Application/Home/Controller/ItemController.class.php
+++ Application/Home/Controller/ItemController.class.php
@@ -8,7 +8,7 @@ class ItemController extends CommonController{
 		$this->is_mobile($_id);
 		//获取商品信息
 		$_item=M('goods')->getIdFind($_id);
-		if(!$_item){
+		if(!$_item || $_item['cid']==7){
 			//如果获取失败 404处理
 			header("location: 404.html");
 			exit();
@@ -31,6 +31,38 @@ class ItemController extends CommonController{
 		
 		$this->display();
 		
+	}
+	//商品详细主页
+	public function adult(){
+		//ID过滤
+		if(isset($_GET['id'])&&is_numeric($_GET['id'])){$_id=$_GET['id'];}else{header("location: 404.html");exit();}
+		//手机访问
+		$this->is_mobile($_id);
+		//获取商品信息
+		$_item=M('goods')->getIdFind($_id);
+		if(!$_item){
+			//如果获取失败 404处理
+			header("location: 404.html");
+			exit();
+		}
+	
+	
+		$_index=$this->getIndex($_item['cid']);//获取游标
+		$_list=M('goods')->getXuSelect(12,"`cid`={$_item['cid']}",'rand()');//随机获取12条数据
+		$_seoTag=implode(",",M('tag')->getTag($_item['title']));  //关键字
+	
+		$this->assign('list',$_list);
+		$this->assign('index',$_index['tag']);
+		$this->assign('do',$_index);
+		$this->assign('item',$_item);
+		$this->assign('small',$this->small($_item['small_images'],$_item['pic_url']));//
+		$_head['title']="{$_item['title']}-我乐购";
+		$_head['key']="{$_item['title']},{$_seoTag},56购物网,淘宝,天猫,特卖,折扣,打折";
+		$_head['des']="【{$_item['title']}】最新特卖打折信息，【{$_item['title']}】最新折扣优惠信息-小编每天为你推荐最新的淘宝、天猫等各大购物商城最优质的折扣商品,享折扣就上我乐购";
+		$this->assign('head',$_head);
+	
+		$this->display();
+	
 	}
 	//如果手机访问
 	public function is_mobile($_id){
