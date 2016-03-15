c848a0ccf8e2bb8b1e88e6d9db8c31fe6c94a097
solody <12943125@qq.com>
2016-03-15 18:23:12 +0800

    网站改名

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -30,9 +30,9 @@ class IndexController extends CommonController{
 		$this->assign('link',$_link);
 		
 		//设置头部
-		$_head['title']='我乐购,折扣购物,打折购物,专业的女性时尚品牌商品导购网';
-		$_head['key']='我乐购,56购物网 ,特卖,折扣,打折';
-		$_head['des']='我乐购,汇聚淘宝、天猫等各大购物商城最优质的折扣商品,每天为你推荐最新的名牌女装、品牌女包、高档女装、潮流服饰、美容护肤品等折扣信息-享折扣就上56购';
+		$_head['title']='女也,她,折扣购物,打折购物,专业的女性时尚品牌商品导购网';
+		$_head['key']='女也,她,56购物网 ,特卖,折扣,打折';
+		$_head['des']='女也,汇聚淘宝、天猫等各大购物商城最优质的折扣商品,每天为你推荐最新的名牌女装、品牌女包、高档女装、潮流服饰、美容护肤品等折扣信息-享折扣就上女也';
 		$this->assign('head',$_head);//模板赋值
 		$this->assign('index','index');//设置导航游标
 		unset($_goods,$_list,$_yifuArray,$_xieziArray,$_baobaoArray,$_peishiArray,$_link,$_head);
